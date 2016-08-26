c03160e836ea7fed50876c887832eb6f67bfe75c
Jonas de Thurah <dethurah@hotmail.com>
2016-08-26 12:18:17 +0000

    Squashed commit of the following:
    commit 15f18f032e317d7c865f853fcce1cf8f9714a8a0
    Author: Jonas de Thurah <dethurah@hotmail.com>
    Date:   Fri Aug 26 14:22:17 2016 +0200
    
        add zero test2
    
    commit 97ef56dd7966181ec1581d6f9a2f774193a36ae2
    Author: Jonas de Thurah <dethurah@hotmail.com>
    Date:   Fri Aug 26 14:10:39 2016 +0200
    
        add zero test
    
    commit be69cfe6d2750171b5b5d3a02594b4963cd98c1e
    Author: Jonas de Thurah <dethurah@hotmail.com>
    Date:   Fri Aug 26 13:26:25 2016 +0200
    
        fix merge problem
    
    commit 1e6d31e44d76ca1fa2f90ce5bcce50ee0c46cebf
    Merge: bd7d03b 3e83432
    Author: Jonas de Thurah <dethurah@hotmail.com>
    Date:   Fri Aug 26 13:21:26 2016 +0200
    
        fix merge problem
    
    commit bd7d03bf0f84a7446dd1e36430cfa6fa33a1a5e0
    Author: Jonas de Thurah <dethurah@hotmail.com>
    Date:   Fri Aug 26 13:10:48 2016 +0200
    
        add all test
    
    commit 39506aea3d1a97b77193bbb606af4d5b44fd4cdc
    Author: Jonas de Thurah <dethurah@hotmail.com>
    Date:   Fri Aug 26 12:58:44 2016 +0200
    
        add M test
    
    commit 32a9689d257bc3c38965bb91f75424c4bd91af1e
    Author: Jonas de Thurah <dethurah@hotmail.com>
    Date:   Fri Aug 26 11:52:49 2016 +0200
    
        add M test
    
    commit 4d841a8b2c5d8337912de7e9e65f35cf6c1b2bd0
    Author: Jonas de Thurah <dethurah@hotmail.com>
    Date:   Fri Aug 26 11:46:22 2016 +0200
    
        add M test
    
    commit 0764b7286fc7c110757aaeb6c329cd9f54a03d71
    Author: Jonas de Thurah <dethurah@hotmail.com>
    Date:   Fri Aug 26 11:43:56 2016 +0200
    
        add M test

--- http_test.go
+++ http_test.go
@@ -235,3 +235,19 @@ func TestRoman47(t *T) {
     }
 
 }
+
+
+func TestRoman0(t *T) {
+    n := romanGenerator(1)
+    r, _ := http.NewRequest("GET", "/roman/0", nil)
+    w := httptest.NewRecorder()
+    n.ServeHTTP(w, r)
+    if w.Code != 200 {
+        t.Fatalf("wrong code returned: %d", w.Code)
+    }
+    body := w.Body.String()
+    if body != fmt.Sprintf("Here's your number: NIL\n") {
+        t.Fatalf("wrong body returned: %s", body)
+    }
+
+}
